80b00af5f3bcccf9865e9f05418433ca13845d6e
Matthew Hayden <matthayden09@gmail.com>
2021-03-28 20:17:44 -0500

    updated Search.js component

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ import './App.css';
 
 function App() {
   const [user, setUser] = useState({})
+  const [username, setUsername] = useState('matthayden09')
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(username)
+  }
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <Search />
+        <Search
+          username={username} setUsername={setUsername}
+          handleSubmit={handleSubmit} />
         <Avatar />
         <Details />
       </header>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 
-const Search = () => {
-    return <div>Search</div>
+const Input = styled.input`
+font-size: 32px;
+padding: 15px;
+border-radius: 10px;
+margin-bottom: 15px;
+`
+
+const Search = ({ username, setUsername, handleSubmit }) => {
+    return (
+        <form onSubmit={handleSubmit}>
+            <Input
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+            />
+        </form>
+    )
 }
 
 export default Search
